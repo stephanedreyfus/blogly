faad0d55348386cba3046294eafec6e859ddb17f
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-20 13:54:34 -0800

    Added static and templates and start of route

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 # db.create_all()
