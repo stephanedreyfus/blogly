cf92772d8a79ec7c79d8fdc107ed0a46501250a9
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-20 14:32:43 -0800

    Add user route and small html changes.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 """Blogly application."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -19,3 +19,19 @@ connect_db(app)
 @app.route('/users/new')
 def add_user_form():
     return render_template("add_user_form.html")
+
+
+@app.route('/users', methods=['POST'])
+def process_user_form():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    img_url = request.form.get('img_url')
+
+    new_user = User(first_name=first_name,
+                    last_name=last_name,
+                    img_url=img_url)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/users')
